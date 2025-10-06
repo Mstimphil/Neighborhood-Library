2d74ec86b3372bafc5ce8d7a35aa23c3c86890d5
Mstimphil <mstimphil@my.yearupunited.org>
2025-10-05 21:32:23 -0400

    string comment

--- src/main/java/com/pluralsight/NeighborhoodLibrary.java
+++ src/main/java/com/pluralsight/NeighborhoodLibrary.java
@@ -1,15 +1,40 @@
 package com.pluralsight;
 
 
+import java.util.Scanner;
+
 public class NeighborhoodLibrary {
-    private static Book[] books = new Book[20];
-    private static int numOfBooks = 20;
+    private static Book[] books = new Book[6];
+    private static int numOfBooks = 6;
     public static void main(String[] args) {
         books[0]= new Book(1, "978-0451524935", "1984", false, "");
+
+        books[1] = new Book(2, "978-0439708180", "Harry Potter and the Sorcerer's Stone", true, "Alice");
+        books[2] =  new Book(3, "978-0141439518", "Pride and Prejudice", false, "");
+        books[3] =  new Book(4, "978-0743273565", "The Great Gatsby", false, "");
+        books[4] = new Book(5, "978-0061120084", "To Kill a Mockingbird", true, "Bob");
+        books[5] = new Book(6, "978-0547928227", "The Hobbit", false, "");
+
+        Scanner neighborhoodLibrary = new Scanner(System.in);
+        String Book = "";
+
+
+
+        while(Book.isCheckedOut()){
+
+            System.out.println("Enter your name: ");
+            books= neighborhoodLibrary.nextLine();
+
+        }
+        neighborhoodLibrary.close();
+
+
 
 
 
     }
 
-        }
+
+
+}
 
